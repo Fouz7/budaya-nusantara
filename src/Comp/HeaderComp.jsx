744faa6c4244522353b6178023f0305dd47d2e71
Fouz7 <dhikifauzan97@gmail.com>
2024-04-14 19:13:06 +0700

    Header Component

---
+++ src/Comp/HeaderComp.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export function HeaderComp() {
+    return (
+        <div>
+            <header>
+                <h1>Your Header Title</h1>
+                <nav>
+                    <ul>
+                        <li>Link 1</li>
+                        <li>Link 2</li>
+                        <li>Link 3</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+export default HeaderComp
---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderComp from '../Comp/HeaderComp'
+
+function Landing() {
+  return (
+    <div>
+        <HeaderComp />
+    </div>
+  )
+}
+
+export default Landing
