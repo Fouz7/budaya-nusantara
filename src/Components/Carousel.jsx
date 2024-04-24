f78a5bc79e545b1af957726695499c808a63194c
Aldrian <vhego811@gmail.com>
2024-04-25 00:13:12 +0100

    Cardcomponent

---
+++ src/Components/Carousel.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Carousel } from '@material-tailwind/react'
+
+
+export function Carousel (){
+    return (
+        <Carousel Classname="Rounded-xl">
+        <img
+        src="*"
+        alt="*"
+        Classname="h-full w-full object-cover"
+        />
+        
+        <img
+        src="*"
+        alt="*"
+        Classname="h-full w-full object-cover"
+        />
+        
+        <img
+        src="*"
+        alt="*"
+        Classname="h-full w-full object-cover"
+        />
+        
+        <img
+        src="*"
+        alt="*"
+        Classname="h-full w-full object-cover"
+        />
+        
+        <img
+        src="*"
+        alt="*"
+        Classname="h-full w-full object-cover"
+        />
+        
+        <img
+        src="*"
+        alt="*"
+        Classname="h-full w-full object-cover"
+        />
+        
+        <img
+        src="*"
+        alt="*"
+        Classname="h-full w-full object-cover"
+        />
+        </Carousel>
+    )
+}
+
+
